74f41de986e9129156243e263af4350385b0b549
Igna Garcia <gnachoxp@gmail.com>
2021-09-23 00:00:07 -0300

    style(general): footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,12 +14,14 @@ import { NotFoundModule } from './pages/notFound/notFount.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
