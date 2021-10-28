651aa43dc7bacfd61fcbe03eecb98ff4ee6ed693
Igna Garcia <gnachoxp@gmail.com>
2021-10-27 21:33:27 -0300

    feat: sections in home

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Launch } from 'src/app/models/launch.model';
+import { Apod } from 'src/app/models/apod.model';
 import { SpaceXService } from 'src/app/services/spacex.service';
+import { ApodService } from '../../services/apod.service'
 
-//TODO: banner
-//TODO: mini calendario estilos y boyones
-//TODO: seccion de ultimo lanzamiento (horizontal)
 //TODO: seccion de galeria (vertical)
 //TODO: seccion de explorar espacio (vertical)
 @Component({
@@ -15,12 +15,52 @@ import { SpaceXService } from 'src/app/services/spacex.service';
 })
 export class IndexComponent {
     launches: Array<Launch>;
+    gallery: Array<Apod>;
+    launch: Launch;
 
-    constructor(private _spaceXService: SpaceXService) {
+    constructor(private _spaceXService: SpaceXService, private router: Router, private _apodService: ApodService) {
         this.launches = new Array<Launch>();
+        this.gallery = new Array<Apod>();
     }
 
     ngOnInit() {
-        this._spaceXService.getUpcomingLaunches().subscribe((res: Array<Launch>) => this.launches.push(...res))
+        this._spaceXService.getLatestLaunches()
+            .subscribe((res: Launch) => this.launch = res)
+        this._spaceXService.getUpcomingLaunches()
+            .subscribe((res: Array<Launch>) => this.launches.push(...res))
+        this._apodService.getApods('2021-10-18', '2021-10-25')
+            .subscribe((res: Array<Apod>) =>  this.gallery.push(...res) );
+    }
+    
+    btnClick(url: string){
+        this.router.navigateByUrl(url)
+    }
+
+    formatDate() {
+        const dateObj = new Date(this.launch.date_unix * 1000);
+        if(this.launch.tbd == true){
+            return 'Indefinida'
+        }
+        let str = `${dateObj.getFullYear()}`
+        if(this.launch.date_precision == 'hour'){
+            str = `${dateObj.getDate()}/${dateObj.getMonth() + 1}/${str} ${dateObj.getHours()}:00`;
+        }
+        else if (this.launch.date_precision == 'day'){
+            str = `${dateObj.getDate()}/${dateObj.getMonth() + 1}/${str}`;
+        }
+        else if (this.launch.date_precision == 'month'){
+            str = `${dateObj.getMonth() + 1}/${str}`;
+        }
+        else if (this.launch.date_precision == 'year'){
+            str = str;
+        }
+        else if (this.launch.date_precision == 'quarter'){
+            str = `${Math.floor((dateObj.getMonth() + 1) / 4)}° Cuarto de ${str}`;
+        }
+        else if (this.launch.date_precision == 'half'){
+            str = `${Math.floor((dateObj.getMonth() + 1) / 2)}° Mitad de ${str}`;
+        }
+        return str;
     }
 }
+ 
